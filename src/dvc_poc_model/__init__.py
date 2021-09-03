b13d66ed1723acb3dde3ee9f54a976141c838735
Osterberger, Moritz (ADAEN-EXTERN) <Moritz.Osterberger.extern@ergo.de>
2021-09-03 12:14:03 +0200

    init

---
+++ src/dvc_poc_model/__init__.py
@@ -0,0 +1,11 @@
+"""
+Toy Model for Ergo DVC POC
+
+In order to test DVC a POC with a test data science usecase is created
+"""
+
+__title__ = "DVC-POC-MODEL"
+
+__copyright__ = "© 2021 Alexander Thamm GmbH"
+
+from .version import __version__
